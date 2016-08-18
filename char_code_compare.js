f7e3d5b2e1a8f09fe77e732971f11b1827cbf772
gomesalexandre <alexandrego2003@gmail.com>
2016-08-19 00:16:43 +0200

    added char_code_compare.js

---
+++ char_code_compare.js
@@ -0,0 +1,19 @@
+function toCode(arg)
+{
+	if ( arg === null || arg === "" || arg.match(/[^a-zA-Z]/g) )
+	{
+		return 0;
+	}
+	return arg.split('').map(function(x)
+	{
+		return x.toUpperCase().charCodeAt(0);
+	}).reduce(function(x,y)
+	{
+		return (x + y);
+	})
+}
+
+function compare(s1, s2)
+{
+	return toCode(s1) === toCode(s2) );
+}
