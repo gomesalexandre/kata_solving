913886c496c58ea8163dca0114c8835075e14480
gomesalexandre <alexandrego2003@gmail.com>
2016-09-09 20:04:37 +0200

    Added hit_count

---
+++ hit_count.php
@@ -0,0 +1,19 @@
+<?php
+function counter_effect($hit_count)
+{
+	$hit_count = str_split($hit_count);
+	$hit_final = array();
+
+	for($i = 0; $i <= count($hit_count); $i++)
+	{
+		$hit_final[$i] = [];
+		for ($j = 0; $j <= $hit_count[$i]; $j++)
+		{
+			array_push($hit_final[$i], $j);
+		}
+	}
+
+	print_r($hit_final);
+
+}
+?>
